7490b605e7a6847199cb6bd67c6f2599bbd70767
Jeremy Nelson <jermnelson@gmail.com>
2026-02-19 10:21:44 -0700

    feat: Separates adminMetadata into its own tests

--- tests/test_monograph.py
+++ tests/test_monograph.py
@@ -10,11 +10,11 @@ TEST_DIRECTORY = pathlib.Path(__file__).parent
 @pytest.mark.parametrize(
     "rdf_file_path, violations, warnings",
     [
-        ("loc/monograph/12516952.cbd.rdf", 29, 2),
-        ("loc/monograph/22932823.cbd.rdf", 22, 2),
-        ("loc/monograph/23703536.cbd.rdf", 16, 3),
-        ("loc/monograph/22483233.cbd.rdf", 16, 3),
-        ("loc/monograph/23694998.cbd.rdf", 16, 3),
+        ("loc/monograph/12516952.cbd.rdf", 5, 2),
+        ("loc/monograph/22932823.cbd.rdf", 6, 2),
+        ("loc/monograph/23703536.cbd.rdf", 0, 3),
+        ("loc/monograph/22483233.cbd.rdf", 0, 3),
+        ("loc/monograph/23694998.cbd.rdf", 0, 3),
     ],
 )
 def test_text_works(
@@ -27,9 +27,7 @@ def test_text_works(
 ):
     """Tests BIBFRAME Text Works"""
     rdf_test_file = TEST_DIRECTORY / rdf_file_path
-    text_graph = dctap_to_shacl("Monograph_Work_Text.tsv")
-    admin_metadata_graph = dctap_to_shacl("Monograph_AdminMetadata.tsv")
-    shacl_graph = admin_metadata_graph + text_graph
+    shacl_graph = dctap_to_shacl("Monograph_Work_Text.tsv")
     results = validate(str(rdf_test_file), shacl_graph=shacl_graph, allow_warnings=True)
     violation_count = int(list(results[1].query(violation_query))[0][0])
     warning_count = int(list(results[1].query(warning_query))[0][0])
@@ -40,11 +38,11 @@ def test_text_works(
 @pytest.mark.parametrize(
     "rdf_file_path, violations, warnings",
     [
-        ("loc/monograph/12516952.cbd.rdf", 27, 1),
-        ("loc/monograph/22932823.cbd.rdf", 18, 1),
-        ("loc/monograph/23703536.cbd.rdf", 16, 1),
-        ("loc/monograph/22483233.cbd.rdf", 16, 1),
-        ("loc/monograph/23694998.cbd.rdf", 16, 1),
+        ("loc/monograph/12516952.cbd.rdf", 3, 1),
+        ("loc/monograph/22932823.cbd.rdf", 2, 1),
+        ("loc/monograph/23703536.cbd.rdf", 0, 1),
+        ("loc/monograph/22483233.cbd.rdf", 0, 1),
+        ("loc/monograph/23694998.cbd.rdf", 0, 1),
     ],
 )
 def test_print_instances(
@@ -57,9 +55,35 @@ def test_print_instances(
 ):
     """Tests BIBFRAME Print Instances"""
     rdf_test_file = TEST_DIRECTORY / rdf_file_path
-    print_graph = dctap_to_shacl("Monograph_Instance_Print.tsv")
-    admin_metadata_graph = dctap_to_shacl("Monograph_AdminMetadata.tsv")
-    shacl_graph = admin_metadata_graph + print_graph
+    shacl_graph = dctap_to_shacl("Monograph_Instance_Print.tsv")
+    results = validate(str(rdf_test_file), shacl_graph=shacl_graph, allow_warnings=True)
+    violation_count = int(list(results[1].query(violation_query))[0][0])
+    warning_count = int(list(results[1].query(warning_query))[0][0])
+    assert violations == violation_count
+    assert warnings == warning_count
+
+
+@pytest.mark.parametrize(
+    "rdf_file_path, violations, warnings",
+    [
+        ("loc/monograph/12516952.cbd.rdf", 24, 0),
+        ("loc/monograph/22932823.cbd.rdf", 16, 0),
+        ("loc/monograph/23703536.cbd.rdf", 16, 0),
+        ("loc/monograph/22483233.cbd.rdf", 16, 0),
+        ("loc/monograph/23694998.cbd.rdf", 16, 0),
+    ],
+)
+def test_monograph_admin_metadata(
+    rdf_file_path: str,
+    violations: int,
+    warnings: int,
+    dctap_to_shacl,
+    violation_query,
+    warning_query,
+):
+    """Tests BIBFRAME Monograph Admin Metadata"""
+    rdf_test_file = TEST_DIRECTORY / rdf_file_path
+    shacl_graph = dctap_to_shacl("Monograph_AdminMetadata.tsv")
     results = validate(str(rdf_test_file), shacl_graph=shacl_graph, allow_warnings=True)
     violation_count = int(list(results[1].query(violation_query))[0][0])
     warning_count = int(list(results[1].query(warning_query))[0][0])
